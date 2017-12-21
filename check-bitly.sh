5d9c68f8ded7663f3fea0556e62c52aa4c598588
Ildar Sagdejev <specious@gmail.com>
2017-12-21 10:42:47 -0500

    Provided special case to validate bitly.com

--- check-bitly.sh
+++ check-bitly.sh
@@ -8,12 +8,33 @@ then
   exit 1
 fi
 
+DOMAIN=$1
+
+show_result() {
+  case $1 in
+    1*) echo "✔ $DOMAIN is a Bitly domain"
+    ;;
+
+    *) echo "✖ $DOMAIN is not a Bitly domain"
+    ;;
+  esac
+}
+
+#
+# bitly.com itself is undetectable using the subsequent technique
+#
+if [[ $DOMAIN == "bitly.com" ]]
+then
+  show_result 1
+  exit 1
+fi
+
 LOCATION_HEADER=$(curl -sI "https://$1/1+" | grep Location)
 CHECK_FOR="https://bitly.com/1+"
 
 case $LOCATION_HEADER in
-  *$CHECK_FOR*) echo "✔ $1 is a Bitly domain"
+  *$CHECK_FOR*) show_result 1
     ;;
-  *) echo "✖ $1 is not a Bitly domain"
+  *) show_result 0
     ;;
 esac
